dbbe4fde6eeec65cf396890a2dc33a36f78ce03f
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-30 17:22:15 +0100

    feat: royalty_splitter

--- royalty_splitter/contracts/royalty_splitter/src/test.rs
+++ royalty_splitter/contracts/royalty_splitter/src/test.rs
@@ -9,7 +9,7 @@ fn test_distribution_and_withdraw() {
     let env = Env::default();
     env.mock_all_auths();
 
-    let contract_id = env.register(RoyaltySplitter);
+    let contract_id = env.register(RoyaltySplitter, ());
 
     let admin = Address::generate(&env);
     let alice = Address::generate(&env);
@@ -33,13 +33,13 @@ fn test_invalid_split() {
     let env = Env::default();
     env.mock_all_auths();
 
-    let contract_id = env.register(RoyaltySplitter);
+    let contract_id = env.register(RoyaltySplitter, ());
 
     let admin = Address::generate(&env);
     let alice = Address::generate(&env);
 
     let mut splits = Map::new(&env);
-    splits.set(alice, 5000); // invalid
+    splits.set(alice, 5000);
 
     env.as_contract(&contract_id, || {
         RoyaltySplitter::init(env.clone(), admin, splits, 10);
